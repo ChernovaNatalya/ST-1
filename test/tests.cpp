0e4fcd0c9c562f43a04a72640bc6124f28ffa399
ChernovaNatalya <73431632+ChernovaNatalya@users.noreply.github.com>
2025-03-16 10:25:23 +0300

    2

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,14 +4,73 @@
 #include <cstdint>
 #include "alg.h"
 
-
+TEST(st1, isPrime1) {
+	uint64_t res = 3;
+	EXPECT_TRUE(checkPrime(res));
+}
+TEST(st1, isPrime2) {
+	uint64_t res = 17;
+	EXPECT_TRUE(checkPrime(res));
+}
+TEST(st1, isPrime3) {
+	uint64_t res = 397;
+	EXPECT_TRUE(checkPrime(res));
+}
+TEST(st1, isNotPrime1) {
+	uint64_t res = 500;
+	EXPECT_FALSE(checkPrime(res));
+}
+TEST(st1, isNotPrime2) {
+	uint64_t res = -3;
+	EXPECT_FALSE(checkPrime(res));
+}
+TEST(st1, nPrime1) {
+	uint64_t res = 3;
+	uint64_t expected = 5;
+	EXPECT_EQ(expected, nPrime(res));
+}
+TEST(st1, nPrime2) {
+	uint64_t res = 15;
+	uint64_t expected = 47;
+	EXPECT_EQ(expected, nPrime(res));
+}
+TEST(st1, nPrime3) {
+	uint64_t res = 35;
+	uint64_t expected = 149;
+	EXPECT_EQ(expected, nPrime(res));
+}
+TEST(st1, nPrime4) {
+	uint64_t res = 168;
+	uint64_t expected = 997;
+	EXPECT_EQ(expected, nPrime(res));
+}
+TEST(st1, nextPrime1) {
+	uint64_t res = 3;
+	uint64_t expected = 5;
+	EXPECT_EQ(expected, nextPrime(res));
+}
+TEST(st1, nextPrime2) {
+	uint64_t res = 383;
+	uint64_t expected = 389;
+	EXPECT_EQ(expected, nextPrime(res));
+}
+TEST(st1, nextPrime3) {
+	uint64_t res = 757;
+	uint64_t expected = 761;
+	EXPECT_EQ(expected, nextPrime(res));
+}
 TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+	uint64_t res = sumPrime(10);
+	uint64_t expected = 17;
+	EXPECT_EQ(expected, res);
 }
 TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+	uint64_t res = sumPrime(30);
+	uint64_t expected = 129;
+	EXPECT_EQ(expected, res);
+}
+TEST(st1, sumPrime3) {
+	uint64_t res = sumPrime(80);
+	uint64_t expected = 791;
+	EXPECT_EQ(expected, res);
 }
